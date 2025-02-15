9fcd2bd80beb88d111676510518ed2bf332d88a1
kenneth <kennethdean08@gmail.com>
2025-02-16 06:12:14 +0800

    edit image remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fourhoi.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
     domains: ["fourhoi.com"],
   },
   reactStrictMode: false,
