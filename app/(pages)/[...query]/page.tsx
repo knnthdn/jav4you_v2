aed486093f8968fc7262e4e2bac0337bf1a29568
kenneth <kennethdean08@gmail.com>
2025-02-16 06:50:30 +0800

    fix suspense when navigate page

--- app/(pages)/[...query]/page.tsx
+++ app/(pages)/[...query]/page.tsx
@@ -25,7 +25,7 @@ export default async function page({
   return (
     <Suspense
       fallback={<SkeletonThumnail />}
-      key={searchParams?.filters || searchParams?.sortby}
+      key={searchParams?.filters || searchParams?.sortby || searchParams?.page}
     >
       <GetQueried query={query} title={title} searchParams={searchParams} />;
     </Suspense>
